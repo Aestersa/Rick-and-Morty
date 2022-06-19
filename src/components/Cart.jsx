2aeb3a995ea7dd6949aa44d6bff2a9373c534296
Aestersa <borya.kaminskiy@gmail.com>
2022-06-19 09:53:22 +0200

    second commit

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,17 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import "../App.css";
 
 const Cart = ({ image, name, status, id }) => {
   return (
-    <a href={`/${id}`}>
-    <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-      <img alt="robots" src={image} />
-      <div>
+    <a className="link" href={`/${id}`}>
+      <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+        <img alt="robots" src={image} />
+        <div>
           <h2>{name}</h2>
-        
-        <p>{status}</p>
+          <p>{status}</p>
+        </div>
       </div>
-    </div>
     </a>
   );
 };
